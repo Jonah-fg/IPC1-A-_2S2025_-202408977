31ba328e07b1a7c522df14fc30fc47f253ef2a9d
Jonás-fg <3812317550101@ingenieria.usac.edu.gt>
2025-09-30 23:24:07 -0600

    [ADD] primer commit: Creacion del modelo MVC y la inicializacion de las vistas: VistaInicioSecion y VistaAdministrador.

--- practica 2/codigo/ArenaUSAC/src/arenausac/controlador/ControladorBatallasPokemon.java
+++ practica 2/codigo/ArenaUSAC/src/arenausac/controlador/ControladorBatallasPokemon.java
@@ -3,7 +3,78 @@ package arenausac.controlador;
 
 import arenausac.modelo.batallasPokemon;
 import arenausac.modelo.personajesPokemon;
+import javax.swing.JTextArea;
 
-
+public class ControladorBatallasPokemon {
+    private int contadorBatallas;
+    private batallasPokemon[] historialBatallas;
+    private int numeroBatallas;
+    private int batallasMax;
     
-}
+    public ControladorBatallasPokemon(int capacidad){
+        this.contadorBatallas = 0;
+        this.historialBatallas = new batallasPokemon[capacidad];
+        this.numeroBatallas = 1;
+        this.batallasMax = capacidad;
+    }
+
+    public void simulacionPeleasPokemon(personajesPokemon pokemon1, personajesPokemon pokemon2, JTextArea Texto){
+        if(pokemon1==null || pokemon2 == null){
+            if(Texto != null){
+                Texto.append("Debes seleccionar 2 Pokemones validos");
+            }
+            return;
+        }
+        if(pokemon1.getPuntosVida()<= 0 || pokemon2.getPuntosVida() <= 0){
+            if(Texto != null){
+                Texto.append("Ambos Pokemones deben estar vivos para pelear");
+            }
+            return;
+        }
+        if(Texto != null){
+            Texto.append("BATALLA POKEMOOOOOOOOOOOOOOOONNsdhj9esdhfcusfhcsd98u");
+            Texto.append(pokemon1.getNombrePokemon()+"vs"+pokemon2.getNombrePokemon());
+        }
+             hilosPokemones primerHilo=new hilosPokemones(pokemon1, pokemon2, Texto);
+            hilosPokemones segundoHilo=new hilosPokemones(pokemon1, pokemon2, Texto);
+            Thread hilo1= new Thread(primerHilo);
+            Thread hilo2=new Thread(segundoHilo);
+            hilo1.start();
+            hilo2.start();
+    }        
+    
+    public void finalizarBatallaPokemones(personajesPokemon pokemon1, personajesPokemon pokemon2, JTextArea Texto){
+        String ganador;
+        
+        if(pokemon1.getPuntosVida() > 0 && pokemon2.getPuntosVida() <= 0){
+            ganador = pokemon1.getNombrePokemon();
+            pokemon1.incrementarBatallasGanadas();
+            pokemon2.incrementarBatallasPerdidas();
+        }
+        else if(pokemon2.getPuntosVida() > 0 && pokemon1.getPuntosVida() <= 0){
+            ganador = pokemon2.getNombrePokemon();
+            pokemon2.incrementarBatallasGanadas();
+            pokemon1.incrementarBatallasPerdidas();
+        } 
+        else {
+            ganador = "Hubo un empateeeededeffkeif";
+        }
+        if(Texto != null){
+            Texto.append("El pokemon ganador es: " + ganador + "\n");
+        }
+        if(contadorBatallas<batallasMax){
+            String bitacora;
+            if(Texto != null) {
+            bitacora = Texto.getText();  
+            } 
+            else {
+            bitacora = "Bitacora inexistente";      
+            }
+            batallasPokemon nuevaBatallaPokemon = new batallasPokemon(numeroBatallas, ganador, pokemon1.getNombrePokemon(), pokemon2.getNombrePokemon(), bitacora);
+            historialBatallas[contadorBatallas] = nuevaBatallaPokemon;
+            contadorBatallas++;
+            numeroBatallas++;
+    }
+            
+    }
+}  
---
+++ proyecto 2/Codigo/Proyecto 2 IPC1/src/proyecto/pkg2/ipc1/Proyecto2IPC1.java
@@ -0,0 +1,10 @@
+
+package proyecto.pkg2.ipc1;
+
+public class Proyecto2IPC1 {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
